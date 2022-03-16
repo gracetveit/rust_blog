78ef51ca23ff1a3b441b25b84b3019d6e76600c3
Grace Tveit <grace.tveit@gmail.com>
2022-03-15 18:58:22 -0700

    feat: now serving a static file

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
-    println!("Hello, world!");
+#[macro_use]
+extern crate rocket;
+
+use rocket::fs::{relative, NamedFile};
+use std::path::PathBuf;
+
+#[get("/")]
+async fn index() -> Option<NamedFile> {
+    let mut path = PathBuf::from(relative!(""));
+    path.pop();
+    path.push("public");
+    if path.is_dir() {
+        path.push("index.html");
+    }
+
+    NamedFile::open(path).await.ok()
+}
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build().mount("/", routes![index])
 }
